6d9f128bc6c147e2092db1c2403e4932bcd377a0
vamsi1 <vamsi.m569@gmail.com>
2018-05-17 13:02:24 +0530

    bash to bash1

--- firstArtifactID/src/test/java/samplePackageFirst/Sample2.java
+++ firstArtifactID/src/test/java/samplePackageFirst/Sample2.java
@@ -2,7 +2,7 @@ package samplePackageFirst;
 
 public class Sample2 {
 public void s1(){
-	// this is next change for git bash
+	// this is next change for git bash1
 	System.out.println("this my second class method");
 }
 }
